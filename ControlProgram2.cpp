5af758a3133f2a4018635a294ca2b53c64d44356
almaudud <almadudiakunngoding@gmail.com>
2026-03-10 20:21:14 +0700

    menambahkan string status lolos yang nanti akann membedakan mana yang akan lolos dan tida lolos

--- ControlProgram2.cpp
+++ ControlProgram2.cpp
@@ -5,3 +5,14 @@ float Rata_rata(float a, float b){
     return (a+b)/2;
 }
 
+string status_lulus(float r){
+    if (r >= 60)
+        return " lolos";
+    else if (r >= 50)
+        return " lolos dipertimbangkan";
+    else if (r >= 40)
+        return "lolos tetapi matrikulasi 1 tahun";
+    else if (r >=30)
+        return "tidak lolos";
+}
+
